e7a1afcb04e5d910902d2ca73dc3acb52c00ee7b
AleDuran96 <durantoledoalbertalejandro@gmail.com>
2025-06-28 23:22:35 -0600

    Codificación de Variables

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 # Obtener la ruta de los datos
-from src.data.load_data import load_data
+from src.data.load_data import load_data, codificar_variables
 from src.config import DATA_PATH_BANK
+import pandas as pd
 
 def main():
-    df = load_data(DATA_PATH_BANK)
-    print(df.head())
+   df = load_data(DATA_PATH_BANK)
+   
+   ''' 
+   El dataframe se analizó en src/Notebooks/exploración
+      -> El nombre de las columnas esta bien escrito
+      -> Los valores de todas las columnas tienen el tipo de dato adecuado, 
+         sin embargo se deben codificar para efectos del modelo.
+      -> No hay nulos para ningún campo
+      -> Se gráfican los campos para ver su distribución
+      -> El dataset no esta equilibrado, dado que para la variable Y el valor
+         de "yes" tiene 521 registros, y el valor "no" tiene 4000.
+   '''
+   
+   # Codificación del DataFrame
+   df_encoded = codificar_variables(df)
+
+   print(df_encoded.head())
 
 if __name__ == "__main__":
     main()
--- src/data/load_data.py
+++ src/data/load_data.py
@@ -4,3 +4,36 @@ from pathlib import Path
 def load_data(path: str) -> pd.DataFrame:
     """Carga un archivo CSV con los datos del banco."""
     return pd.read_csv(Path(path), delimiter=";")
+
+def codificar_variables(df: pd.DataFrame) -> pd.DataFrame:
+    df_encoded = df.copy()
+
+    # Codificar variables binarias (yes/no)
+    binarias = ["default", "housing", "loan", "y"]
+    for col in binarias:
+        df_encoded[col] = df_encoded[col].map({"yes": 1, "no": 0})
+
+    # Ordinal - education
+    edu_map = {
+        "unknown": -1,  # o podrías usar imputación
+        "primary": 0,
+        "secondary": 1,
+        "tertiary": 2
+    }
+    df_encoded["education"] = df_encoded["education"].map(edu_map)
+
+    # Ordinal - month
+    month_map = {
+        "jan": 1, "feb": 2, "mar": 3, "apr": 4, "may": 5, "jun": 6,
+        "jul": 7, "aug": 8, "sep": 9, "oct": 10, "nov": 11, "dec": 12
+    }
+    df_encoded["month"] = df_encoded["month"].map(month_map)
+
+    # Resto de categóricas a dummies
+    categ_dummies = ["job", "marital", "contact", "poutcome"]
+    df_encoded = pd.get_dummies(df_encoded, columns=categ_dummies, drop_first=True)
+    
+    # Convertir booleanos a enteros
+    df_encoded = df_encoded.astype(int)
+    
+    return df_encoded
